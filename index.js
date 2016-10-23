c2bfede86a172f2394c9814f61402830b242d10f
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-10-23 20:05:43 +0800

    express static 方法改变，直接用app.use(express.static('./public'));将 public 作为根目录

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express();
-app.use('./static', express.static('public'));
+// app.use('./static', express.static('public'));
+app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
     res.send('hello world!');
