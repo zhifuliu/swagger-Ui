8032c69fa38212dff563954b10dcc2e2794c750c
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-10-23 22:35:37 +0800

    不能实现自动刷新页面，这是一个node 项目，并不像前端项目，所以加了一个伪自动刷新，详情见 README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     plumber = require('gulp-plumber'),
-    livereload = require('gulp-livereload');
-
+    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('develop', function() {
     livereload.listen();
@@ -19,6 +19,14 @@ gulp.task('develop', function() {
         this.stdout.pipe(process.stdout);
         this.stderr.pipe(process.stderr);
     });
-});
 
+    browserSync.init({
+        port: 9001,
+        server: {
+            baseDir: './public/'
+        },
+        index: './index.html',
+        files: '**/*.*'
+    });
+});
 gulp.task('default', ['develop']);
